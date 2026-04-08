ac65110f8901b8cb564f164c54a26d67e03f1f99
Josh <50963558+JoshhSandhu@users.noreply.github.com>
2026-04-09 03:31:36 +0530

    for testing

---
+++ Runtime/codebase/SolanaMobileStack/JsonRpcClient/Responses/CapabilitiesResult.cs
@@ -0,0 +1,26 @@
+using Newtonsoft.Json;
+using UnityEngine.Scripting;
+
+// ReSharper disable once CheckNamespace
+
+namespace Solana.Unity.SDK
+{
+    [Preserve]
+    public class CapabilitiesResult
+    {
+        [JsonProperty("supports_clone_authorization")]
+        public bool? SupportsCloneAuthorization { get; set; }
+
+        [JsonProperty("max_transactions_per_request")]
+        public int? MaxTransactionsPerRequest { get; set; }
+
+        [JsonProperty("max_messages_per_request")]
+        public int? MaxMessagesPerRequest { get; set; }
+
+        [JsonProperty("supported_transaction_versions")]
+        public string[] SupportedTransactionVersions { get; set; }
+
+        [Preserve]
+        public CapabilitiesResult() { }
+    }
+}
